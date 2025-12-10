5fa165e590858df40bc6ed46612e5a888c67a6c4
Matthew Kuntz <kuntz09.matthew@gmail.com>
2025-12-10 12:57:12 -0600

    feat: add account balances UI to main page

---
+++ electron/SEMANTIC_RELEASE.md
@@ -0,0 +1,14 @@
+# Semantic Release configuration for Financial Assistant App
+# Run this from the electron directory:
+#   npx semantic-release
+#
+# This will:
+# - Analyze commit messages for version bump (major, minor, patch)
+# - Update package.json and CHANGELOG.md
+# - Create a GitHub release with changelog
+# - Push changes to GitHub
+#
+# To use, make sure your commits follow Conventional Commits:
+#   feat: for new features
+#   fix: for bug fixes
+#   BREAKING CHANGE: for breaking changes
--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+
+const { app, BrowserWindow, dialog, Menu } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('path');
+
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -11,6 +14,56 @@ function createWindow() {
     }
   });
   win.loadFile(path.join(__dirname, '../frontend/index.html'));
+
+  // App menu with update option
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: [
+        { role: 'quit' }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Check for Updates',
+          click: () => {
+            autoUpdater.checkForUpdatesAndNotify();
+            dialog.showMessageBox(win, {
+              type: 'info',
+              title: 'Update Check',
+              message: 'Checking for updates...'
+            });
+          }
+        }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+
+  // Auto-update logic
+  win.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox(win, {
+      type: 'info',
+      title: 'Update Available',
+      message: 'A new version is available. It will be downloaded and installed automatically.'
+    });
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox(win, {
+      type: 'info',
+      title: 'Update Ready',
+      message: 'Update downloaded. The app will restart to install the update.'
+    }).then(() => {
+      autoUpdater.quitAndInstall();
+    });
+  });
 }
 
 app.whenReady().then(createWindow);
